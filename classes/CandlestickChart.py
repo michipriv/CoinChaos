4fd4058fecb4ed49c873f904ca79a153d379f360
Michael <michi@dermader.com>
2023-12-18 23:02:03 +0100

    Update

--- classes/CandlestickChart.py
+++ classes/CandlestickChart.py
@@ -26,41 +26,50 @@ class CandlestickChart:
 
 
 
-    def plot_candlestick(self, df, symbol, interval):
-        # Überprüfen, ob EMA- und Vektorfarben-Spalten vorhanden sind
-        if 'ema_50' not in df.columns or 'ema_100' not in df.columns or 'vector_color' not in df.columns:
-            raise ValueError("Eine oder mehrere erforderliche Spalten ('ema_50', 'ema_100', 'vector_color') fehlen im DataFrame.")
-
-        # DataFrame vorbereiten
+    def prepare_dataframe(self, df):
         df['Date'] = pd.to_datetime(df['time'], unit='ms')
         df['Date'] = df['Date'].apply(mdates.date2num)
+        return df
 
-        # Figure und Axes erstellen
-        fig, ax = plt.subplots()
-
-        # Jede Kerze zeichnen
+  
+    def draw_candles(self, ax, df):
         for idx, row in df.iterrows():
-            # Farbe aus der 'vector_color'-Spalte verwenden
             candle_color = row['vector_color']
-            
-            # Kerzenkörper
-            ax.plot([row['Date'], row['Date']], [row['low_price'], row['high_price']], color='black')  # Docht
-            ax.plot([row['Date'], row['Date']], [row['open_price'], row['close_price']], color=candle_color, linewidth=8)  # Kerzenkörper
 
-        # EMA-50 und EMA-100 Linien zeichnen
+            # Bestimmen der Docht-Farbe basierend auf der 'Kerze'-Spalte
+            if row['Kerze'] == 'Grün':
+                wick_color = 'green'
+            elif row['Kerze'] == 'Rot':
+                wick_color = 'red'
+            else:
+                wick_color = 'black'  # Standardfarbe, falls 'Kerze' weder 'Grün' noch 'Rot' ist
+
+            # Docht zeichnen
+            ax.plot([row['Date'], row['Date']], [row['low_price'], row['high_price']], color=wick_color)
+
+            # Kerzenkörper zeichnen
+            ax.plot([row['Date'], row['Date']], [row['open_price'], row['close_price']], color=candle_color, linewidth=8)
+
+
+
+    def draw_ema_lines(self, ax, df):
         ax.plot(df['Date'], df['ema_50'], label='EMA-50', color='blue', linewidth=2)
         ax.plot(df['Date'], df['ema_100'], label='EMA-100', color='purple', linewidth=2)
 
-        # X-Achse als Datum formatieren
+    def plot_candlestick(self, df, symbol, interval):
+        if 'ema_50' not in df.columns or 'ema_100' not in df.columns or 'vector_color' not in df.columns:
+            raise ValueError("Eine oder mehrere erforderliche Spalten fehlen im DataFrame.")
+
+        df = self.prepare_dataframe(df)
+        fig, ax = plt.subplots()
+        self.draw_candles(ax, df)
+        self.draw_ema_lines(ax, df)
+
         ax.xaxis_date()
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
         plt.xticks(rotation=45)
-
-        # Titel, Legende und Achsenbeschriftungen
         plt.title(f'{symbol} Candlestick Chart ({interval})')
         plt.xlabel('Date')
         plt.ylabel('Price')
         plt.legend()
-
         plt.show()
-
